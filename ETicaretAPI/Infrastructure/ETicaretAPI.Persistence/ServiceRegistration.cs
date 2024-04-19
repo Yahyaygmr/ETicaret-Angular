6460c108bd50a9c1162917ba62c809808f3db440
Yahyaygmr <yahyaygmr@gmail.com>
2024-04-19 13:01:01 +0300

    created repeository patterns abstracts, concretes, and registrations

--- ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -1,6 +1,12 @@
 ﻿
+using ETicaretAPI.Application.Repositories.CustomerRepositories;
+using ETicaretAPI.Application.Repositories.OrderRepositories;
+using ETicaretAPI.Application.Repositories.ProductRepositories;
 using ETicaretAPI.Persistence.Configurations;
 using ETicaretAPI.Persistence.Context;
+using ETicaretAPI.Persistence.Repositories.CustomerRepositories;
+using ETicaretAPI.Persistence.Repositories.OrderRepositories;
+using ETicaretAPI.Persistence.Repositories.ProductRepositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,12 +22,18 @@ namespace ETicaretAPI.Persistence
     {
         public static void AddPersistenceServices(this IServiceCollection services)
         {
-            ConfigurationManager configurationManager = new();
-            configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Presentation/ETicaretAPI.API"));
-            configurationManager.AddJsonFile("appsettings.json");
-
             services.AddDbContext<ETicaretAPIDbContext>(options =>
             options.UseSqlServer(Configuration.ConnectionString));
+
+            services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
+            services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
+
+            services.AddScoped<IOrderReadRepository, OrderReadRepository>();
+            services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
+
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+
         }
     }
 }
