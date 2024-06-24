e0e0309f6dd435a14b442b571bc906d7c03a4198
Yahya Yağmur <yahyaygmr@gmail.com>
2024-06-24 15:19:10 +0300

    The product adding page and process have been configured in the client.

--- ETicaretClient/src/app/admin/components/products/products.component.ts
+++ ETicaretClient/src/app/admin/components/products/products.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
-import { Product } from 'src/app/contracts/product';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
@@ -16,18 +15,5 @@ export class ProductsComponent extends BaseComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.showSpinner(SpinnerType.BallAtom);
-
-    this.httpClientService.get<Product>({
-      controller: "products"
-    }).subscribe(data => console.log(data));
-
-    // this.httpClientService.post({
-    //   controller: "products"
-    // }, {
-    //   name: "Kalem",
-    //   stock: 100,
-    //   price: 15
-    // }).subscribe();
   }
 }
